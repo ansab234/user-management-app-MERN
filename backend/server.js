6f9be6be9cdd41de7eea38e852e624bd52913095
ansab234 <ansabali067@gmail.com>
2022-10-13 01:11:04 +0500

    heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/mongodb");
 const userRoutes = require("./routes/userRoute");
+const path = require("path");
 
 const cors = require("cors");
 
@@ -14,6 +15,24 @@ connectDB();
 app.use(express.json());
 
 app.use("/", userRoutes);
+
+// --------------------------deployment------------------------------
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
 
 const PORT = process.env.PORT || 5000;
 
